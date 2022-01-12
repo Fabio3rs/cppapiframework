614c9e04241c27214e823ea68c264585ff65b778
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2022-01-11 22:23:21 -0300

    auto gen job list script

---
+++ scripts/genjoblist.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DIR="jobs"
+mkdir "autogen"
+
+pushd "${DIR}"
+FILES=$(find -iname '*.hpp')
+popd
+
+echo "$(clang-format - <<EOF
+#pragma once
+$(
+for f in $FILES
+do 
+    CLASSNAME=$(echo $f | awk -F '.hpp' '{ print $1 }' | awk -F '/' '{ print $2 }')
+	echo "#include \"../jobs/$CLASSNAME.hpp\""
+done
+)
+
+namespace autogen{
+static void registerJobs(job::JobsHandler &handler){
+    $(
+for f in $FILES
+do 
+    CLASSNAME=$(echo $f | awk -F '.hpp' '{ print $1 }' | awk -F '/' '{ print $2 }')
+	echo "handler.register_job_handler<job::$CLASSNAME>();"
+done
+)
+}
+};
+
+EOF
+)" > "autogen/jobslist.hpp"
+
