d583c817fa9624fc8094efcdf5a5763de5755c7c
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2023-03-21 18:50:46 -0300

    Added Mail.hpp

---
+++ src/Mail/Mail.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../TimeHandler.h"
+#include <Poco/Net/HTMLForm.h>
+#include <Poco/Net/MailMessage.h>
+#include <sstream>
+
+namespace mailbase {
+
+struct Mail {
+
+    void build() {
+        Poco::Net::MailMessage message;
+
+        std::stringstream sstr;
+
+        TimeHandler msg;
+        Poco::Net::HTMLForm form;
+        msg.stringify(sstr, form);
+
+        message.setContentType("text/html; charset=UTF-8");
+        message.setContent(sstr.str(), Poco::Net::MailMessage::ENCODING_8BIT);
+
+        message.setSubject("subject");
+        message.write(std::cout);
+        std::cout << std::endl;
+    }
+};
+
+} // namespace mailbase
